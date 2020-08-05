204967a040995bf341f52bdefa7edef5d69c926d
Elizabeth <evorotnicoff@gmail.com>
2020-08-05 05:19:55 -0300

    finish-exc-ten

---
+++ exce-ten/src/ar/com/ada/online/second/projectname/Main.java
@@ -0,0 +1,38 @@
+package ar.com.ada.online.second.projectname;
+
+import com.sun.java.swing.plaf.motif.MotifTreeUI;
+import com.sun.org.apache.xpath.internal.objects.XString;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        String holidayOne = "mexico";
+        String holidayTwo = "acapulco";
+        String holidayThree = "cancun";
+        double km;
+        double money;
+
+
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.print("Ingrese el precio por km: ");
+        km = keyboard.nextDouble();
+
+        System.out.print("Ingrese la cantidad de dinero que tiene: ");
+        money = keyboard.nextDouble();
+
+        if (money >= (km *1200 *2)){
+            System.out.println(holidayThree);
+        } else if (money >= (km * 800 *2)){
+            System.out.println(holidayTwo);
+        } else if (money >= (km *750 *2)){
+            System.out.println(holidayOne);
+        }else {
+            System.out.println("Quedate en casa");
+        }
+    }
+
+
+}
